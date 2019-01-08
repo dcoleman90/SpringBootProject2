ecbd2f27fb6a3af310956ede2fc73d8faa40feb6
dcoleman90 <dcolem12@my.westga.edu>
2019-01-08 15:11:01 -0500

    Added PUT and DELETE methods - the PUT will remove the old topic and replace it with the added one

--- src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -12,41 +12,45 @@ public class TopicService {
 
 	@Autowired
 	private TopicRepository topicRepostitory;
-	
-	private List<Topic> topics = new ArrayList<>(Arrays.asList(new Topic("Spring", "Spring Framework", "Spring Framework Description"),
-			new Topic("A New Hope", "Episode 4", " It is a period of Civil war"),
-			new Topic("Empire Strikes Back", "Episode 5", "It is a dark time for the Rebellion"),
-			new Topic("Return of the Jedi", "Episode 6", "Luke Skywalker has returned to his home planet of Tatooine")));
 
-	
-	
+	private List<Topic> topics = new ArrayList<>(
+			Arrays.asList(new Topic("Spring", "Spring Framework", "Spring Framework Description"),
+					new Topic("A New Hope", "Episode 4", " It is a period of Civil war"),
+					new Topic("Empire Strikes Back", "Episode 5", "It is a dark time for the Rebellion"),
+					new Topic("Return of the Jedi", "Episode 6",
+							"Luke Skywalker has returned to his home planet of Tatooine")));
+
 	public List<Topic> getAllTopics() {
 		List<Topic> topicList = new ArrayList<>();
 		topicRepostitory.findAll().forEach(topicList::add);
-		
+
 		return topicList;
 	}
-	
-	
+
 	public void addTopic(Topic addedTopic) {
 		this.topicRepostitory.save(addedTopic);
 	}
 
 	public Topic getTopic(String id) {
 		return topicRepostitory.findById(id).orElse(null);
-		
+
 	}
-	
+
+	/**
+	 * TopicRepiository is advanced enough that it looks for identical Id and can update them 
+	 * 
+	 * An issue came when a user updated the id - this method would simply create a NEW topic with the id and leave the old one
+	 * The if loop deletes any issues with this and uses the more traditional update method - removing the old id and replacing it with a new one
+	 */
 	public void updateTopic(Topic updatedTopic, String id) {
-		for (int count = 0; count < this.topics.size(); count ++) {
-			if (this.topics.get(count).getId().equals(id)) {
-				this.topics.set(count, updatedTopic);
-			}
+		if(this.topicRepostitory.findById(id).isPresent()) {
+			this.topicRepostitory.deleteById(id);
 		}
+		this.topicRepostitory.save(updatedTopic);
 	}
-	
+
 	public void deleteTopic(String id) {
-		this.topics.removeIf(topic -> topic.getId().equals(id));
+		this.topicRepostitory.deleteById(id);
 	}
 
 }
