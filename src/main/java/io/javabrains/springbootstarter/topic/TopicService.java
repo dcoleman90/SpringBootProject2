2ffcc2384fa0f52ebc5af42262fd5bc8d4bf668a
dcoleman90 <dcolem12@my.westga.edu>
2019-01-08 14:59:47 -0500

    Can now add to topic database and get a topic from the database

--- src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -4,28 +4,37 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Service
 public class TopicService {
 
+	@Autowired
+	private TopicRepository topicRepostitory;
+	
 	private List<Topic> topics = new ArrayList<>(Arrays.asList(new Topic("Spring", "Spring Framework", "Spring Framework Description"),
 			new Topic("A New Hope", "Episode 4", " It is a period of Civil war"),
 			new Topic("Empire Strikes Back", "Episode 5", "It is a dark time for the Rebellion"),
 			new Topic("Return of the Jedi", "Episode 6", "Luke Skywalker has returned to his home planet of Tatooine")));
 
+	
+	
 	public List<Topic> getAllTopics() {
-		return topics;
+		List<Topic> topicList = new ArrayList<>();
+		topicRepostitory.findAll().forEach(topicList::add);
+		
+		return topicList;
 	}
 	
 	
 	public void addTopic(Topic addedTopic) {
-		this.topics.add(addedTopic);
+		this.topicRepostitory.save(addedTopic);
 	}
 
 	public Topic getTopic(String id) {
-		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		return topicRepostitory.findById(id).orElse(null);
+		
 	}
 	
 	public void updateTopic(Topic updatedTopic, String id) {
